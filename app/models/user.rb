41c83544589b9287d7f6e1c0fbcf669620cfeeb4
keima-d08 <doikeima@gmail.com>
2022-03-16 17:09:37 +0900

    userモデルバリデーションの記入（再）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+         VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+         VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+
 
          validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
          validates :email, presence: true
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+         validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
+         validates :first_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
+         validates :last_name_kana, presence: true, format: { with: VALID_NAME_KANA_REGEX }
          validates :birthday, presence: true
 end
