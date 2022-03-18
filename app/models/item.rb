955bb1ee2692ebbd00c39974c0ba51e66cb4f55b
keima-d08 <doikeima@gmail.com>
2022-03-18 14:46:39 +0900

    アクティブハッシュの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,20 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+
   validates :name,             presence: true, length: { maximum: 40 }
   validates :content,          presence: true, length: { maximum: 1000 }
-  validates :category_id,      presence: true
-  validates :status_id,        presence: true
-  validates :postage_id,       presence: true
-  validates :prefecture_id,    presence: true
-  validates :days_to_ship_id,  presence: true
+  validates :category_id,      presence: true, numericality: { other_than: 1 }
+  validates :status_id,        presence: true, numericality: { other_than: 1 }
+  validates :postage_id,       presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
+  validates :days_to_ship_id,  presence: true, numericality: { other_than: 1 }
   validates :price,            presence: true
   validates :image,            presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_17_142957) do
+ActiveRecord::Schema.define(version: 2022_03_18_033140) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 2022_03_17_142957) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "days_to_ships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "content", null: false
@@ -46,6 +56,21 @@ ActiveRecord::Schema.define(version: 2022_03_17_142957) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "postages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
