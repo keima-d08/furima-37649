29794cc1a1ce9f7d25cd62e4f533ac517635816f
keima-d08 <doikeima@gmail.com>
2022-03-19 23:31:49 +0900

    単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name                    {'テスト'}
+    content                 {'テスト'}
+    category_id             {'2'}
+    status_id               {'2'}
+    postage_id              {'2'}
+    prefecture_id           {'2'}
+    days_to_ship_id         {'2'}
+    price                   {'500'}
     
+    association :user 
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
