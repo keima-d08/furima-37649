2bc86990dc74c3b92d3a04e938a4576425ff44e2
keima-d08 <doikeima@gmail.com>
2022-03-21 22:44:29 +0900

    バリデーション編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,11 @@
 class BuysController < ApplicationController
 
-  def show
+  def index
+    @item = Item.find(params[:item_id])
   end
-  
+
+  def create
+
+  end
+
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,13 +1,15 @@
 class Destination < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/
+  VALID_PHONENUMBER_REGEX =/\A[0-9]+\z/
   
-  validates :postcode,          presence: true
-  validates :prefecture_id,     presence: true
+  validates :postcode,          presence: true, length: { maximum: 8 }, format: { with: VALID_POSTCODE_REGEX }
+  validates :prefecture_id,     presence: true, numericality: { other_than: 1 }
   validates :municipalities,    presence: true
   validates :address,           presence: true
   validates :building_name
-  validates :phone_number,      presence: true
+  validates :phone_number,      presence: true, length: { maximum: 11 }, format: { with: VALID_PHONENUMBER_REGEX }
 
   belongs_to :buyer
 
