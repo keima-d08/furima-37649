94a218a38ba3d97dd15a7dcf58bb2e3dcd76a4f2
keima-d08 <doikeima@gmail.com>
2022-03-21 17:01:10 +0900

    buyerモデル作成

---
+++ app/models/buyer.rb
@@ -0,0 +1,8 @@
+class Buyer < ApplicationRecord
+  validates :user,          presence: true
+  validates :item,          presence: true
+
+  belongs_to :user
+  belongs_to :item
+  has_one :destination
+end
