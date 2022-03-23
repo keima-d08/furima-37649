df5f2e905c1b2b4436adb92212ddd2c47ff784ee
keima-d08 <doikeima@gmail.com>
2022-03-23 23:59:59 +0900

    クレジットカード決済機能実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -16,6 +16,12 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
       @buyer_destination = BuyerDestination.new(buyer_params)
     if @buyer_destination.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buyer_params[:token],
+        currency: 'jpy'
+      )
        @buyer_destination.save
        redirect_to root_path
     else
@@ -28,7 +34,8 @@ class BuysController < ApplicationController
   private
 
   def buyer_params
-    params.require(:buyer_destination).permit(:postcode, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id,  item_id: params[:item_id])
+    params.require(:buyer_destination).permit(:postcode, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
 
 end
--- app/models/buyer_destination.rb
+++ app/models/buyer_destination.rb
@@ -1,6 +1,6 @@
 class BuyerDestination
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :municipalities, :address, :building_name, :phone_number, :item_id, :user_id
+  attr_accessor :postcode, :prefecture_id, :municipalities, :address, :building_name, :phone_number, :user_id, :item_id, :token
   
   VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/
   VALID_PHONENUMBER_REGEX =/\A[0-9]+\z/
@@ -10,9 +10,9 @@ class BuyerDestination
   validates :municipalities,    presence: true
   validates :address,           presence: true
   validates :phone_number,      presence: true, length: { maximum: 11 }, format: { with: VALID_PHONENUMBER_REGEX }
-         
   validates :user_id,           presence: true
   validates :item_id,           presence: true
+  validates :token, presence: true
 
   def save
     buyer = Buyer.create(item_id: item_id, user_id: user_id)
