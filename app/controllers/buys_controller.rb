30cffb6c193534618dd827420e189467b236db71
keima-d08 <doikeima@gmail.com>
2022-03-24 14:44:15 +0900

    テストコード実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -16,12 +16,7 @@ class BuysController < ApplicationController
     @item = Item.find(params[:item_id])
       @buyer_destination = BuyerDestination.new(buyer_params)
     if @buyer_destination.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: buyer_params[:token],
-        currency: 'jpy'
-      )
+       pay_item
        @buyer_destination.save
        redirect_to root_path
     else
@@ -35,6 +30,15 @@ class BuysController < ApplicationController
 
   def buyer_params
     params.require(:buyer_destination).permit(:postcode, :prefecture_id, :municipalities, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+   
+  def pay_item
+   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buyer_params[:token],
+        currency: 'jpy'
+      )
   end
 
 
--- app/models/buyer_destination.rb
+++ app/models/buyer_destination.rb
@@ -5,14 +5,14 @@ class BuyerDestination
   VALID_POSTCODE_REGEX = /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}\z/
   VALID_PHONENUMBER_REGEX =/\A[0-9]+\z/
   
-  validates :postcode,          presence: true, length: { maximum: 8 }, format: { with: VALID_POSTCODE_REGEX }
+  validates :postcode,          presence: true, length: { is: 8 }, format: { with: VALID_POSTCODE_REGEX }
   validates :prefecture_id,     presence: true, numericality: { other_than: 1 }
   validates :municipalities,    presence: true
   validates :address,           presence: true
-  validates :phone_number,      presence: true, length: { maximum: 11 }, format: { with: VALID_PHONENUMBER_REGEX }
+  validates :phone_number,      presence: true, length: { in: 10..11 }, format: { with: VALID_PHONENUMBER_REGEX }
   validates :user_id,           presence: true
   validates :item_id,           presence: true
-  validates :token, presence: true
+  validates :token,             presence: true
 
   def save
     buyer = Buyer.create(item_id: item_id, user_id: user_id)
